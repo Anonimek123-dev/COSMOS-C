8f8b600a2b8392a9b99924bd4c50ce6a833cd0f8
Anonimek123-dev <maksymilian.zdan123@gmail.com>
2025-10-06 20:32:52 +0200

    Create irq.c

---
+++ COSMOS-C/Core/arch/x86_64/IRQ/irq.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include "HAL/console/print.h"
+#include "arch/x86_64/PIC/pic.h"
+#include "Drivers/PS2/keyboard/ps2.h"
+
+// Forward declare PIC send EOI and keyboard handler
+extern void pic_send_eoi(unsigned char irq);
+extern void keyboard_irq_handler(void);
+
+// C handler called from assembly with vector in rdi
+void isr_handler(uint64_t vector) {
+    if (vector <= 31) {
+        // CPU exception
+        print_str("Exception vector: ");
+        print_int((int)vector);
+        print_str("\n");
+        // For now: just return (or halt) - in dev you might halt
+        // __asm__ volatile ("cli; hlt");
+        return;
+    }
+
+    if (vector >= 32 && vector <= 47) {
+        unsigned char irq = (unsigned char)(vector - 32);
+        // dispatch common IRQs here
+        switch (irq) {
+            case 1: // keyboard
+                keyboard_irq_handler();
+                break;
+            default:
+                // other IRQs we don't handle yet
+                break;
+        }
+
+        // send EOI to PICs
+        pic_send_eoi(irq);
+        return;
+    }
+
+    // unexpected vector
+    print_str("Unhandled vector: ");
+    print_int((int)vector);
+    print_str("\n");
+}
+
+// enable interrupts (wrapper)
+void enable_irq(void) {
+    __asm__ volatile ("sti");
+}
