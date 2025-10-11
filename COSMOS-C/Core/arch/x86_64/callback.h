e3c50966ae5efdaf9017be6cdc0c659a126844fc
Anonimek123-dev <maksymilian.zdan123@gmail.com>
2025-10-11 18:16:48 +0200

    SOmething

--- COSMOS-C/Core/arch/x86_64/callback.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#define MAX_CALLBACKS 16
-
-typedef void (*timer_callback_t)(void);
-void set_timeout(timer_callback_t cb, uint64_t ms);
-void timer_callbacks_update(void);
